b0bb84d6a209f04552f48818aaa9150efd660560
nonaninona <starcraft0529@gmail.com>
2024-07-25 21:08:55 +0900

    [이코테] 큰 수의 법칙 풀이2 추가

--- 이코테/큰수의법칙.py
+++ 이코테/큰수의법칙.py
@@ -7,13 +7,22 @@ arr.sort()
 first = arr[-1]
 second = arr[-2]
 
-ret = 0
-for i in range(1, m+1):
-  if i % (k+1) == 0:
-    ret += second
-  else:
-    ret += first
+# 풀이 1
+# ret = 0
+# for i in range(1, m+1):
+#   if i % (k+1) == 0:
+#     ret += second
+#   else:
+#     ret += first
 
-print(ret)
+# print(ret)
 
 # 내가 책보다 잘 짠듯 뿌듯
+# 이 문제에서 M이 10,000이 아니라 100억 이라면?? -> 10억이 1초니까 logN 뿐인데..?
+
+# 풀이 2
+firstCount = m//(k+1) * k + m%(k+1)
+secondCount = m//(k+1)
+ret = first * firstCount + second * secondCount
+
+print(ret)
