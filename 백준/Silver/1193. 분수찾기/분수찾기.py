b33c77c6dcb8c13a4f02c9d703c647294cac6d1f
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-08 08:40:57 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,18 +1,15 @@
 x = int(input())
-n = 1
-while True:
-  if n * (n+1) / 2 >= x:
-    break
-  n+=1
 
-if (n+1)%2 == 0:
-  order = x - ((n-1)*n // 2)
-  denominator = order
-  numerator = n + 1 - denominator
-  print(str(numerator) + "/" + str(denominator))
+diagonalOrder = 1
+while x > diagonalOrder:
+  x -= diagonalOrder
+  diagonalOrder += 1
 
+if diagonalOrder % 2 == 0:
+  top = x
+  bottom = diagonalOrder + 1 - x
+  print(f"{top}/{bottom}")
 else:
-  order = x - ((n-1)*n // 2)
-  numerator = order
-  denominator = n + 1 - numerator
-  print(str(numerator) + "/" + str(denominator))
+  top = diagonalOrder + 1 - x
+  bottom = x
+  print(f"{top}/{bottom}")
