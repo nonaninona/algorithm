47198782b795a98ead3afec5eec595c37af90540
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-08 10:32:26 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -5,11 +5,12 @@ while x > diagonalOrder:
   x -= diagonalOrder
   diagonalOrder += 1
 
+top, bottom = 0, 0
 if diagonalOrder % 2 == 0:
   top = x
   bottom = diagonalOrder + 1 - x
-  print(f"{top}/{bottom}")
 else:
   top = diagonalOrder + 1 - x
   bottom = x
-  print(f"{top}/{bottom}")
+
+print(f"{top}/{bottom}")
