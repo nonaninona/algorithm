e20c58b2b5d7a65ae358749915bc478d0108edc3
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-15 13:17:14 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 232 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,5 +1,6 @@
-N = int(input())
-dots = [tuple(map(int, input().split())) for i in range(N)]
+import sys
+N = int(sys.stdin.readline().rstrip())
+dots = [tuple(map(int, sys.stdin.readline().rstrip().split())) for i in range(N)]
 dots.sort(key = lambda x: (x[0], x[1]))
 for (x, y) in dots:
-    print(x, y)
+    sys.stdout.write(str(x) + " " + str(y) + "\n")
