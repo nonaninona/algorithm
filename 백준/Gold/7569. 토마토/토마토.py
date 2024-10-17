6545aca1ddb040ad1527d6793303455dcd78f4af
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-17 13:05:22 +0900

    [Gold V] Title: 토마토, Time: 4168 ms, Memory: 101400 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,27 +3,19 @@ import sys
 
 M, N, H = map(int, sys.stdin.readline().rstrip().split())
 
-boxes = []
-tomatoes = []
+boxes = [[list(map(int, sys.stdin.readline().rstrip().split())) for i in range(N)] for j in range(H)]
+queue = deque()
+
+dx = [-1, 0, 1, 0, 0, 0]
+dy = [0, 1, 0, -1, 0, 0]
+dz = [0, 0, 0, 0, 1, -1]
 
 for k in range(H):
-    box = []
     for i in range(N):
-        b = list(map(int, sys.stdin.readline().rstrip().split()))
         for j in range(M):
-            if b[j] == 1:
-                tomatoes.append((k, i, j, 1))
-                b[j] = 0
-        box.append(b)
-    boxes.append(box)
-    
-queue = deque()
-dx = [-1, 0, 1,  0, 0,  0]
-dy = [ 0, 1, 0, -1, 0,  0]
-dz = [ 0, 0, 0,  0, 1, -1]
-
-for tomato in tomatoes:
-    queue.append(tomato)
+            if boxes[k][i][j] == 1:
+                queue.append((k, i, j, 1))
+                boxes[k][i][j] = 0
 
 while queue:
     (z, y, x, d) = queue.popleft()
