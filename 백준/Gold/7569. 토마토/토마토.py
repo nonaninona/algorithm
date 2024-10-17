8910a14b242057ab4a91c0b8f8c225c3d29f1334
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-17 13:01:26 +0900

    [Gold V] Title: 토마토, Time: 4336 ms, Memory: 102492 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,8 +2,6 @@ from collections import deque
 import sys
 
 M, N, H = map(int, sys.stdin.readline().rstrip().split())
-visited = [[[False for j in range(M)] for i in range(N)] for k in range(H)]
-distance = [[[0 for j in range(M)] for i in range(N)] for k in range(H)]
 
 boxes = []
 tomatoes = []
@@ -14,10 +12,11 @@ for k in range(H):
         b = list(map(int, sys.stdin.readline().rstrip().split()))
         for j in range(M):
             if b[j] == 1:
-                tomatoes.append((k, i, j, 0))
+                tomatoes.append((k, i, j, 1))
+                b[j] = 0
         box.append(b)
     boxes.append(box)
-
+    
 queue = deque()
 dx = [-1, 0, 1,  0, 0,  0]
 dy = [ 0, 1, 0, -1, 0,  0]
@@ -30,14 +29,10 @@ while queue:
     (z, y, x, d) = queue.popleft()
     if z < 0 or z >= H or y < 0 or y >= N or x < 0 or x >= M:
         continue
-    if visited[z][y][x]:
-        continue
-    if boxes[z][y][x] == -1:
-        visited[z][y][x] = True
+    if boxes[z][y][x] != 0:
         continue
 
-    visited[z][y][x] = True
-    distance[z][y][x] = d
+    boxes[z][y][x] = d
     for i in range(6):
         queue.append((z+dz[i], y+dy[i], x+dx[i], d+1))
 
@@ -46,10 +41,9 @@ for k in range(H):
     for i in range(N):
         for j in range(M):
             t = boxes[k][i][j]
-            d = distance[k][i][j]
-            if t == 0 and d == 0:
+            if t == 0:
                 print(-1)
                 exit(0)
-            if t != -1 and d > maximum:
-                maximum = d
-print(maximum)
+            if t != -1 and t > maximum:
+                maximum = t
+print(maximum-1)
