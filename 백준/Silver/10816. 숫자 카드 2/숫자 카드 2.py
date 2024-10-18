5b104b61105765b05c998ad9aab90da047174e1f
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-10-19 02:45:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 652 ms, Memory: 136492 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,25 +1,14 @@
 import sys
 N = int(sys.stdin.readline().rstrip())
 cards = list(map(int, sys.stdin.readline().rstrip().split()))
-M = int(sys.stdin.readline().rstrip())
-originalFinds = list(map(int, sys.stdin.readline().rstrip().split()))
-finds = originalFinds[:]
 counts = {}
-for f in finds:
-    counts[f] = 0
-cards.sort()
-finds.sort()
+for c in cards:
+    if c in counts:
+        counts[c] += 1
+    else:
+        counts[c] = 1
 
-j = 0
-for i in range(len(finds)):
-    find = finds[i]
-    while True:
-        if j == len(cards):
-            break
-        if find == cards[j]:
-            counts[find] = counts[find] + 1
-        if find < cards[j]:
-            break
-        j+=1
-for o in originalFinds:
-    sys.stdout.write(str(counts[o]) + " ")
+M = int(sys.stdin.readline().rstrip())
+finds = list(map(int, sys.stdin.readline().rstrip().split()))
+for f in finds:
+    sys.stdout.write(str(counts.get(f, 0)) + " ")
