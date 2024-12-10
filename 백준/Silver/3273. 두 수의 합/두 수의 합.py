1b1909d924cf3dac82fed7eb5a230c526e7efeac
nonaninona <53222283+nonaninona@users.noreply.github.com>
2024-12-10 11:02:52 +0900

    [Silver III] Title: 두 수의 합, Time: 92 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,16 +1,18 @@
 import sys
-isExist = [False for i in range(2_000_000)]
-
 N = int(input())
 nums = list(map(int, sys.stdin.readline().rstrip().split()))
-for num in nums:
-    isExist[num] = True
+nums.sort()
 target = int(input())
 
 count = 0
-for i in range(1, target // 2 + 1):
-    if i == target - i:
-        continue
-    if isExist[i] and isExist[target-i]:
+start = 0
+end = N-1
+while start < end:
+    if nums[start] + nums[end] == target:
         count += 1
+        end -= 1
+    elif nums[start] + nums[end] < target:
+        start += 1
+    elif nums[start] + nums[end] > target:
+        end -= 1
 print(count)
