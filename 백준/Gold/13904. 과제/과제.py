93d66a98eadc8608cd486f5c849c58be1d898ee6
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-02-16 14:26:25 +0900

    [Gold III] Title: 과제, Time: 64 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.py
@@ -0,0 +1,23 @@
+import heapq
+
+N = int(input())
+works = [list() for i in range(1001)]
+
+for _ in range(N):
+    d, w = map(int, input().split())
+    works[d].append(w)
+# print(works)
+
+h = []
+heapq.heapify(h)
+works.reverse()
+
+ans = 0
+for i, w in enumerate(works):
+    if i == len(works)-1:
+        break
+    for num in w:
+        heapq.heappush(h, -1 * num)
+    if len(h) > 0:
+        ans += -1 * heapq.heappop(h)
+print(ans)
