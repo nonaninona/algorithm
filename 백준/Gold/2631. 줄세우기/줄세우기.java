847d4a8d72dc0fd569f9452532c1509b117c2bfd
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-07-17 11:55:19 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     static int N;
     static int[] nums;
-    static int[] dp;
+    static List<Integer> tail;
 
     public static void main(String[] args) throws Exception {
 
@@ -13,25 +15,40 @@ public class Main {
         N = Integer.parseInt(br.readLine());
 
         nums = new int[N];
-        dp = new int[N];
+        tail = new ArrayList<>();
 
         for(int i=0;i<N;i++) {
             nums[i] = Integer.parseInt(br.readLine());
         }
 
-
-        int ret = 0;
-        dp[0] = 1;
+        tail.add(nums[0]);
         for(int i=1;i<N;i++) {
-            int length = 0;
-            for(int j=0;j<i;j++) {
-                if(nums[j] < nums[i])
-                    length = Math.max(dp[j], length);
-            }
-            dp[i] = length+1;
-            ret = Math.max(dp[i], ret);
+            calc(i);
         }
 
-        System.out.println(N - ret);
+        System.out.println(N - tail.size());
+    }
+
+    private static int calc(int i) {
+        int num = nums[i];
+
+        if(tail.get(tail.size()-1) < num) {
+            tail.add(num);
+            return tail.size();
+        }
+
+        int lo = -1;
+        int hi = tail.size();
+        while(lo + 1 < hi) {
+            int mid = (lo+hi)/2;
+            if(!(num <= tail.get(mid)))
+                lo = mid;
+            else
+                hi = mid;
+        }
+
+        tail.set(hi, num);
+
+        return hi+1;
     }
 }
