3f1423a8409d6088f87ee4079360edbc0ece0c1a
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-07-22 11:14:54 +0900

    [Silver I] Title: 악보는 거들 뿐, Time: 388 ms, Memory: 68292 KB -BaekjoonHub

--- 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.java
+++ 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.java
@@ -16,45 +16,22 @@ public class Main {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
+        int upStreak = 1;
+        int downStreak = 1;
         int ret = 1;
-        int count = 1;
-        int prevD = 0;
-//        System.out.println(count);
 
-        if(M > 1) {
-            int curD = nums[1] - nums[0];
-            if(curD == 0) {
-                count = 1;
-            } else {
-                count = 2;
-            }
-            prevD = curD;
-            ret = Math.max(ret, count);
-        }
-//        System.out.println(count);
-
-        for(int i=2;i<M;i++) {
-            int curD = nums[i] - nums[i-1];
-            if(0 < curD) {
-                if(0 < prevD) {
-                    count++;
-                } else {
-                    count = 2;
-                }
-            } else if(curD < 0) {
-                if(prevD < 0) {
-                    count++;
-                } else {
-                    count = 2;
-                }
+        for(int i=1;i<M;i++) {
+            int d = nums[i] - nums[i-1];
+            if(d > 0) {
+                upStreak++;
+                downStreak = 1;
+            } else if(d < 0) {
+                upStreak = 1;
+                downStreak++;
             }
 
-//            System.out.println(count);
-
-            ret = Math.max(ret, count);
-
-            if(curD != 0)
-                prevD = curD;
+            ret = Math.max(ret, upStreak);
+            ret = Math.max(ret, downStreak);
         }
 
         System.out.println(ret);
