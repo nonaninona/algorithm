0df7212a6c9107975cc79c890f6e6298bacc8445
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-07-28 11:41:20 +0900

    [Gold IV] Title: 사이클 게임, Time: 620 ms, Memory: 125412 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] parent;
+
+    public static int find(int x) {
+        if(parent[x] == x)
+            return parent[x];
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int a, int b) {
+        int aP = find(a);
+        int bP = find(b);
+
+        if(aP < bP)
+            parent[bP] = aP;
+        else
+            parent[aP] = bP;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N];
+        for(int i=0;i<N;i++)
+            parent[i] = i;
+
+        int ret = 0;
+        for(int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(find(a) == find(b)) {
+                ret = i+1;
+                break;
+            }
+            else
+                union(a, b);
+        }
+
+        System.out.println(ret);
+    }
+}
