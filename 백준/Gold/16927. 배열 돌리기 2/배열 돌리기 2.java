bc5c5dbbd9e34beaf3c09f7fdd250d3091ae2cbc
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-07-31 09:47:54 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 420 ms, Memory: 33484 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -7,7 +7,6 @@ public class Main {
     static int[][] A;
 
     public static void main(String[] args) throws FileNotFoundException, Exception {
-//		System.setIn(new FileInputStream("Test4.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -16,23 +15,23 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
 
         A = new int[N][M];
-        for(int i=0;i<N;i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;j++) {
+            for (int j = 0; j < M; j++) {
                 A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int times = Math.min(N/2,  M/2);
-        for(int i=0;i<times;i++) {
+        int times = Math.min(N / 2, M / 2);
+        for (int i = 0; i < times; i++) {
             int cycleLength = 2 * (N - 2 * i) + 2 * (M - 2 * i) - 4;
 //            System.out.println(cycleLength);
-            rotate(i, R%cycleLength);
+            rotate(i, R % cycleLength, cycleLength);
         }
 
         StringBuilder builder = new StringBuilder();
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<M;j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 builder.append(A[i][j]).append(" ");
             }
             builder.append("\n");
@@ -40,19 +39,27 @@ public class Main {
         System.out.println(builder);
     }
 
-    private static void rotate(int l, int r) {
+    private static void rotate(int l, int r, int cl) {
 //        System.out.println(N + " " + M + " " + r + " " + l);
-        for(int k=0;k<r;k++) {
-            int temp = A[l][l];
-            for(int j=l;j<M-1-l;j++)
-                A[l][j] = A[l][j+1];
-            for(int i=l;i<N-1-l;i++)
-                A[i][M-1-l] = A[i+1][M-1-l];
-            for(int j=M-1-l;j>l;j--)
-                A[N-1-l][j] = A[N-1-l][j-1];
-            for(int i=N-1-l;i>l+1;i--)
-                A[i][l] = A[i-1][l];
-            A[l+1][l] = temp;
-        }
+        List<Integer> list = new ArrayList<>(cl);
+        for (int j = l; j < M - 1 - l; j++)
+            list.add(A[l][j]);
+        for (int i = l; i < N - 1 - l; i++)
+            list.add(A[i][M - 1 - l]);
+        for (int j = M - 1 - l; j > l; j--)
+            list.add(A[N - 1 - l][j]);
+        for (int i = N - 1 - l; i > l; i--)
+            list.add(A[i][l]);
+        Collections.rotate(list, -r);
+
+        int idx = 0;
+        for (int j = l; j < M - 1 - l; j++)
+            A[l][j] = list.get(idx++);
+        for (int i = l; i < N - 1 - l; i++)
+            A[i][M - 1 - l] = list.get(idx++);
+        for (int j = M - 1 - l; j > l; j--)
+            A[N - 1 - l][j] = list.get(idx++);
+        for (int i = N - 1 - l; i > l; i--)
+            A[i][l] = list.get(idx++);
     }
 }
