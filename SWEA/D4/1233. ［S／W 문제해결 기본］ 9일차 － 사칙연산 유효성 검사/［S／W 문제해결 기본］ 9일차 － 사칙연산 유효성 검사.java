e3f45b1cc313a174a0454bb2eb97abd9cab935f2
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-08-04 12:14:26 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 91 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+import static java.nio.file.Files.move;
+
+public class Solution {
+
+    static int N;
+
+    static class Node {
+        String operator = "";
+        int number = -1;
+        int left = -1;
+        int right = -1;
+    }
+
+    static Node[] tree;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int tc=1;tc<10+1;tc++) {
+            N = Integer.parseInt(br.readLine());
+
+            tree = new Node[N+1];
+            StringTokenizer st;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int idx = Integer.parseInt(st.nextToken());
+                tree[idx] = new Node();
+                String n = st.nextToken();
+                if (n.equals("+") || n.equals("-") || n.equals("/") || n.equals("*")) {
+                    tree[idx].operator = n;
+                } else {
+                    tree[idx].number = Integer.parseInt(n);
+                }
+
+                int order = 0;
+                while (st.hasMoreTokens()) {
+                    if (order == 0) {
+                        tree[idx].left = Integer.parseInt(st.nextToken());
+                        order++;
+                    } else {
+                        tree[idx].right = Integer.parseInt(st.nextToken());
+                    }
+                }
+            }
+
+            System.out.println("#" + tc + " " + (dfs(1) ? 1 : 0));
+        }
+
+
+    }
+
+    private static boolean dfs(int n) {
+        int left = tree[n].left;
+        int right = tree[n].right;
+        int number = tree[n].number;
+
+//        System.out.println(n + " " + number + " " + left + " " + right);
+        if(left == -1 && right == -1) {
+//            System.out.println(n + " " + (number == -1));
+            return number != -1;
+        }
+
+        String oper = tree[n].operator;
+        boolean ret = true;
+        if(left != -1)
+            ret &= dfs(left);
+        if(right != -1)
+            ret &= dfs(right);
+
+//        System.out.println(n + " " + ret);
+        return ret;
+    }
+}
