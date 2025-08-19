c38cc57f9df16573b0079e8e0c5b6b6f2b9908ba
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-08-19 12:04:49 +0900

    [D2] Title: 나무 높이, Time: 100 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,47 +1,54 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 
-    static int N, T;
-    static int[] heights;
+	static int N, T;
+	static int[] trees;
+	static int max, oneCnt, twoCnt, ans;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int tc=1;tc<=T;tc++) {
+			
+			N = Integer.parseInt(br.readLine());
+			
+			trees = new int[N];
+			st = new StringTokenizer(br.readLine());
+			
+			max = Integer.MIN_VALUE;
+			for(int i=0;i<N;i++) {
+				trees[i] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, trees[i]);
+			}
+			
+			oneCnt = 0; twoCnt = 0;
+			for(int i=0;i<N;i++) {
+				int r = max - trees[i];
+				oneCnt += r%2;
+				twoCnt += r/2;
+			}
+			
+			// System.out.println(oneCnt + " " + twoCnt);
+			
+			int diff = twoCnt - oneCnt;
+			if(diff >= 2) {
+				int change = (diff+1)/3;
+				twoCnt -= change;
+				oneCnt += 2*change;
+			} 
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
+			if(twoCnt >= oneCnt) {
+				ans = twoCnt * 2;
+			} else if(twoCnt < oneCnt) {
+				ans = (oneCnt-1) * 2 + 1;
+			}
+			
+			System.out.printf("#%d %d\n", tc, ans);
+		}
+	}
 
-        for(int tc=1;tc<T+1;tc++) {
-            N = Integer.parseInt(br.readLine());
-
-            heights = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int max = 0;
-            for (int i = 0; i < N; i++) {
-                heights[i] = Integer.parseInt(st.nextToken());
-                max = Math.max(max, heights[i]);
-            }
-
-            int twoCount = 0;
-            int oneCount = 0;
-            for (int i = 0; i < N; i++) {
-                int remain = max - heights[i];
-                twoCount += remain / 2;
-                oneCount += remain % 2;
-            }
-
-            if(oneCount > twoCount) {
-                System.out.println("#" + tc + " " + (oneCount * 2 - 1));
-            } else {
-                int total = (twoCount * 2 + oneCount);
-                int d = total / 3;
-                int r = total % 3;
-                if(r == 0)
-                    System.out.println("#" + tc + " " + d * 2);
-                else if(r == 1)
-                    System.out.println("#" + tc + " " + (d*2+1));
-                else if(r == 2)
-                    System.out.println("#" + tc + " " + ((d+1) * 2));
-            }
-        }
-    }
 }
