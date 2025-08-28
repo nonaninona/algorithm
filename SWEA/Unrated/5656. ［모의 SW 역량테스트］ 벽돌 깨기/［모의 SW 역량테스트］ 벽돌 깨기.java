ffea3269043f4df0200497437562e3012f3d0817
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-08-28 10:37:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 200 ms, Memory: 96,296 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -11,7 +11,7 @@ class Solution {
         T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        for(int tc=1;tc<=T;tc++) {
+        for (int tc = 1; tc <= T; tc++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
@@ -20,69 +20,63 @@ class Solution {
             int[][] board;
             board = new int[H][W];
             ans = 0;
-            for(int i=0;i<H;i++) {
+            for (int i = 0; i < H; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j=0;j<W;j++) {
+                for (int j = 0; j < W; j++) {
                     board[i][j] = Integer.parseInt(st.nextToken());
-                    if(board[i][j] != 0)
+                    if (board[i][j] != 0)
                         ans++;
                 }
             }
 
-//            System.out.println("board");
-//            for(int i=0;i<H;i++) {
-//                System.out.println(Arrays.toString(board[i]));
-//            }
-//            System.out.println(ans);
-//            System.out.println();
-
             dfs(0, ans, board);
+            
             System.out.printf("#%d %d\n", tc, ans);
         }
     }
 
-    private static void dfs(int depth, int count, int[][] board) {
-        if(depth == N) {
-            ans = Math.min(ans, count);
+    private static void dfs(int depth, int remain, int[][] board) {
+        if (depth == N) {
+            ans = Math.min(ans, remain);
             return;
         }
 
-        for(int j=0;j<W;j++) {
+        if(remain == 0) {
+            ans = 0;
+            return;
+        }
+
+        if(ans == 0)
+            return;
+
+        for (int j = 0; j < W; j++) {
             int[][] nBoard = new int[H][W];
-            for(int i=0;i<H;i++)
+            for (int i = 0; i < H; i++)
                 nBoard[i] = Arrays.copyOf(board[i], W);
             int removeCnt = initBomb(nBoard, j);
-            nBoard = drop(nBoard);
-//            System.out.println();
-//            System.out.println("depth " + depth + " pos " + j + " rmCount " + removeCnt);
-//            for(int i=0;i<H;i++)
-//                System.out.println(Arrays.toString(nBoard[i]));
-//            System.out.println();
-            dfs(depth+1, count - removeCnt, nBoard);
+            drop(nBoard);
+            
+            dfs(depth + 1, remain - removeCnt, nBoard);
+
+            if(ans == 0)
+                return;
         }
     }
 
-    private static int[][] drop(int[][] board) {
-        int[][] nBoard = new int[H][W];
-
-        for(int j=0;j<W;j++) {
-            int zeroCnt = 0;
-            for(int i=H-1;i>=0;i--) {
-                if(board[i][j] == 0)
-                    zeroCnt++;
-            }
-            for(int i=0;i<zeroCnt;i++)
-                nBoard[i][j] = 0;
-            int idx = zeroCnt;
-            for(int i=0;i<H;i++) {
-                if(board[i][j] != 0) {
-                    nBoard[idx][j] = board[i][j];
-                    idx++;
+    private static void drop(int[][] board) {
+        for (int j = 0; j < W; j++) {
+            int idx = H - 1;
+            for (int i = H - 1; i >= 0; i--) {
+                if (board[i][j] != 0) {
+                    if (idx != i) {
+                        board[idx][j] = board[i][j];
+                        board[i][j] = 0;
+                    }
+                    idx--;
                 }
+
             }
         }
-
-        return nBoard;
     }
 
     static int[] Dy = {-1, 0, 1, 0};
@@ -90,8 +84,8 @@ class Solution {
 
     private static int initBomb(int[][] board, int pos) {
         int rmCnt = 0;
-        for(int i=0;i<H;i++) {
-            if(board[i][pos] != 0) {
+        for (int i = 0; i < H; i++) {
+            if (board[i][pos] != 0) {
                 rmCnt += bomb(board, i, pos);
                 break;
             }
@@ -104,14 +98,14 @@ class Solution {
         board[y][x] = 0;
 
         int cnt = 1;
-        for(int d=0;d<4;d++) {
+        for (int d = 0; d < 4; d++) {
             int idx = 1;
-            int ny = y+Dy[d], nx = x+Dx[d];
+            int ny = y + Dy[d], nx = x + Dx[d];
             while (!(ny < 0 || nx < 0 || ny >= H || nx >= W)) {
-                if(idx == range)
+                if (idx == range)
                     break;
 
-                if(board[ny][nx] != 0)
+                if (board[ny][nx] != 0)
                     cnt += bomb(board, ny, nx);
 
                 ny += Dy[d];
