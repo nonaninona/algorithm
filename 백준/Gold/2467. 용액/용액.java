fb58c99dbb34353e75242d820447e89ea47b56a1
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-09-07 19:05:55 +0900

    [Gold V] Title: 용액, Time: 332 ms, Memory: 28432 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -17,55 +17,27 @@ public class Main {
         for (int i = 0; i < N; i++)
             values[i] = Integer.parseInt(st.nextToken());
 
-        ans = Integer.MAX_VALUE;
-        for(int i=0;i<N;i++) {
-            int v1 = values[i];
-            int v2 = bSrc(i);
+        int left = vans1 = 0;
+        int right = vans2 = N - 1;
+        ans = Math.abs(values[left] + values[right]);
 
-//            System.out.println(v1 + " " + v2);
-
-            if(Math.abs(v1 + v2) < ans) {
-                ans = Math.abs(v1 + v2);
-                if(v1 < v2) {
-                    vans1 = v1;
-                    vans2 = v2;
-                } else {
-                    vans1 = v2;
-                    vans2 = v1;
-                }
+        while (left != right) {
+            int sum = values[left] + values[right];
+            if (Math.abs(sum) < ans) {
+                ans = Math.abs(sum);
+                vans1 = left;
+                vans2 = right;
             }
-        }
 
-        System.out.printf("%d %d", vans1, vans2);
-    }
-
-    private static int bSrc(int idx) {
-        int v = values[idx];
-
-        int lo = -1;
-        int hi = N;
-
-        while(lo + 1 < hi) {
-            int mid = (lo+hi)/2;
-            if(-v <= values[mid])
-                hi = mid;
+            if (sum < 0)
+                left++;
+            else if (sum > 0)
+                right--;
             else
-                lo = mid;
+                break;
+
         }
 
-        if(lo == idx)
-            lo -= 1;
-        if(hi == idx)
-            hi += 1;
-
-        if(lo <= -1)
-            return values[hi];
-        if(hi >= N)
-            return values[lo];
-
-        if(Math.abs(v + values[hi]) < Math.abs(v + values[lo]))
-            return values[hi];
-        else
-            return values[lo];
+        System.out.println(values[vans1] + " " + values[vans2]);
     }
 }
