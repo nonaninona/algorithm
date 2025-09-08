3e14ac956a5a418e894ef92a5e4aebbc704c590e
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-09-08 09:37:13 +0900

    [D3] Title: 0/1 Knapsack, Time: 110 ms, Memory: 29,068 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -6,46 +6,37 @@ public class Solution {
     static int T, N, K;
     static int[] V, C;
     static int[] dp;
+    static StringBuilder builder = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
 
-        V = new int[100];
-        C = new int[100];
-        dp = new int[1001];
         StringTokenizer st;
         for (int tc = 1; tc <= T; tc++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
+            V = new int[N];
+            C = new int[N];
+            dp = new int[K+1];
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 V[i] = Integer.parseInt(st.nextToken());
                 C[i] = Integer.parseInt(st.nextToken());
             }
 
-
-            Arrays.fill(dp, 0);
             for (int i = 0; i < N; i++) {
-                for (int j = K; j >= 1; j--) {
-                    if(V[i] <= j)
-                        dp[j] = Math.max(dp[j], C[i]);
-
-                    if (j - V[i] >= 0) {
-                        dp[j] = Math.max(dp[j], dp[j - 1]);
-                        dp[j] = Math.max(dp[j], dp[j - V[i]] + C[i]);
-                    }
-                    else
-                        dp[j] = Math.max(dp[j], dp[j - 1]);
-
+                for (int j = K; j >= V[i]; j--) {
+                    dp[j] = Math.max(dp[j], dp[j - V[i]] + C[i]);
                 }
             }
 
 //            System.out.println(Arrays.toString(dp));
-            System.out.printf("#%d %d\n", tc, dp[K]);
+            builder.append("#").append(tc).append(" ").append(dp[K]).append("\n");
         }
+        System.out.println(builder);
 
     }
 
