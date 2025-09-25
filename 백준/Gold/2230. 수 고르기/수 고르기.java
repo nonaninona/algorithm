b9f03244a72d4a8763a06997f8273f92a6e45953
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-09-25 21:58:25 +0900

    [Gold V] Title: 수 고르기, Time: 344 ms, Memory: 27472 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N, M;
+    static int[] A;
+    static int ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[N];
+        for(int i=0;i<N;i++)
+            A[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(A);
+
+        ans = Integer.MAX_VALUE;
+        int right = 0;
+        int left = 0;
+        while(right < N && left < N) {
+            int diff = A[right] - A[left];
+            if(diff < M)
+                right++;
+            else {
+                left++;
+                ans = Math.min(ans, diff);
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+
+}
