64dfd06d41f34f127d0eacbd539b788d235717c1
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-11-26 10:22:13 +0900

    [Gold II] Title: 게임, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.java
+++ 백준/Gold/1103. 게임/게임.java
@@ -6,7 +6,7 @@ public class Main {
     static int N, M;
     static char[][] B;
     static boolean[][] V;
-    static boolean[][][] dp;
+    static int[][] dp;
     static int ans;
 
     static int[] Dy = {-1, 1, 0, 0};
@@ -27,30 +27,60 @@ public class Main {
         }
 
         ans = 0;
-        dp = new boolean[N][M][N*M+1];
+
         V = new boolean[N][M];
         V[0][0] = true;
-        dfs(0, 0, 1);
 
-        System.out.println(ans);
+        dp = new int[N][M];
+        for(int i=0;i<N;i++)
+            Arrays.fill(dp[i], -1);
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<M;j++) {
+                if(B[i][j] == 'H')
+                    continue;
+
+                boolean isBase = true;
+                for(int k=0;k<4;k++) {
+                    int ny = i + (B[i][j] - '0') * Dy[k];
+                    int nx = j + (B[i][j] - '0') * Dx[k];
+                    if (ny < 0 || nx < 0 || ny >= N || nx >= M || B[ny][nx] == 'H')
+                        continue;
+                    isBase = false;
+                    break;
+                }
+
+                if(isBase)
+                    dp[i][j] = 0;
+            }
+        }
+
+//        for (int i = 0; i < N; i++) {
+//            System.out.println(Arrays.toString(dp[i]));
+//        }
+
+        dfs(0, 0);
+
+        if(ans == -1)
+            System.out.println(-1);
+        else
+            System.out.println(dp[0][0] + 1);
 
     }
 
-    private static void dfs(int y, int x, int c) {
-        ans = Math.max(ans, c);
-        if(dp[y][x][c])
-            return;
-        dp[y][x][c] = true;
-//        System.out.println(y + " " + x + " " + c + " " + ans);
+    private static int dfs(int y, int x) {
+        if(ans == -1)
+            return -1;
 
+        if(dp[y][x] != -1)
+            return dp[y][x];
+
+        int ret = 0;
         for(int i=0;i<4;i++) {
             if(ans == -1)
-                return;
+                return -1;
 
             int ny = y + (B[y][x]-'0') * Dy[i];
             int nx = x + (B[y][x]-'0') * Dx[i];
-
-//            System.out.println(ny + " " + nx);
 
             if(ny < 0 || nx < 0 || ny >= N || nx >= M)
                 continue;
@@ -58,12 +88,14 @@ public class Main {
                 continue;
             if(V[ny][nx]) {
                 ans = -1;
-                return;
+                return -1;
             }
 
             V[ny][nx] = true;
-            dfs(ny, nx, c+1);
+            ret = Math.max(ret, 1 + dfs(ny, nx));
             V[ny][nx] = false;
         }
+
+        return dp[y][x] = ret;
     }
 }
