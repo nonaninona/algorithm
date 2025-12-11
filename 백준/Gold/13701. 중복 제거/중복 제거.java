1c6bee78923129d6787127a5d26fbec9aff0a39d
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-12-11 23:22:50 +0900

    [Gold IV] Title: 중복 제거, Time: 10636 ms, Memory: 363088 KB -BaekjoonHub

--- 백준/Gold/13701. 중복 제거/중복 제거.java
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -11,15 +12,22 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 //        System.out.println((1 << 20) * 32);
-        int[] isShow = new int[(1 << 20)];
+//        int[] isShow = new int[(1 << 20)];
+        BitSet bits  = new BitSet((1 << 25) + 1);
         StringTokenizer st = new StringTokenizer(br.readLine());
         while(st.hasMoreTokens()) {
             Integer num = Integer.parseInt(st.nextToken());
-            if((isShow[num/32] & (1 << (num%32))) == 0) {
+            if(!bits.get(num)){
                 System.out.print(num + " ");
-                isShow[num/32] = isShow[num/32] | (1 << (num%32));;
+                bits.set(num);
             }
+//            if((isShow[num/32] & (1 << (num%32))) == 0) {
+//                System.out.print(num + " ");
+//                isShow[num/32] = isShow[num/32] | (1 << (num%32));;
+//            }
         }
+
+
     }
 
 }
